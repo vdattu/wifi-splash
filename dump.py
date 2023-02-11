930591292de2ba45742b69fdff4197779db76555
vdattu <105353357+vdattu@users.noreply.github.com>
2023-02-11 13:27:58 +0530

    Add files via upload

---
+++ dump.py
@@ -0,0 +1,27 @@
+from flask import Flask,request
+import json,time
+import os
+import wget 
+app = Flask(__name__)
+#@app.route('/',methods=["GET","POST","DELETE"])
+@app.route('/<string:id>',methods=["GET","POST","DELETE"])
+def dat_traf(id):
+    if request.method=='POST':
+        data=request.data
+        dat_lst=eval(data)
+        print(dat_lst)
+        if dat_lst:
+            path=os.path.join("C:/Users/Acer/Desktop/wifi-splash/resources/image",id)
+            os.remove(path)
+            for j in dat_lst:
+                wget.download(j,'C://Users//Acer//Desktop//wifi-splash//resources//image')
+                print("list downloaded")
+            return "--successfully downloaded-----"
+        else:
+            return "NO Data Found"
+    return "This App is Running & FIles Downloading Successfully"
+        
+    
+if (__name__)=="__main__":
+    app.run(host='0.0.0.0',port='5002')
+
